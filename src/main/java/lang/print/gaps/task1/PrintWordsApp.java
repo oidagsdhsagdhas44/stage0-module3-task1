1bd989aee46413629b0371eb2d4f47dc7f20c5e7
oidagsdhsagdhas44 <tornike.buchukuri@gau.edu.ge>
2023-02-17 01:40:10 +0400

    fix fill gaps and print words

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("java\nis\ngood");
+        System.out.println("java\nis\nsmth");
     }
 }
